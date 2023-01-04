b8522ee1a43ab44326bf7afcda5939df59049c3b
jrainpre <jonathan.rainprechter+42@gmail.com>
2023-01-04 09:12:24 +0100

    added env lst function (untested)

--- safe_env_in_lst.c
+++ safe_env_in_lst.c
@@ -1,10 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   safe_env_in_lst.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrainpre <jrainpre@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 08:58:54 by jrainpre          #+#    #+#             */
+/*   Updated: 2023/01/04 08:59:37 by jrainpre         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-void fill_env_lst(t_env_list **env_lst, char **envp)
+void	fill_env_lst(t_env_list **env_lst, char **envp)
 {
-	int i;
-	t_env_list *temp;
-	t_env_list *new;
+	int			i;
+	t_env_list	*temp;
+	t_env_list	*new;
 
 	i = 0;
 	temp = *env_lst;
@@ -12,7 +24,8 @@ void fill_env_lst(t_env_list **env_lst, char **envp)
 	{
 		new = malloc(sizeof(t_env_list));
 		new->name = ft_substr(envp[i], 0, ft_strchr(envp[i], '=') - envp[i]);
-		new->value = ft_substr(envp[i], ft_strchr(envp[i], '=') - envp[i] + 1, ft_strlen(envp[i]));
+		new->value = ft_substr(envp[i], ft_strchr(envp[i], '=') - envp[i] + 1,
+				ft_strlen(envp[i]));
 		new->next = NULL;
 		if (i == 0)
 			*env_lst = new;
@@ -23,9 +36,9 @@ void fill_env_lst(t_env_list **env_lst, char **envp)
 	}
 }
 
-char *get_env_value(t_env_list *env_lst, char *name)
+char	*get_env_value(t_env_list *env_lst, char *name)
 {
-	t_env_list *temp;
+	t_env_list	*temp;
 
 	temp = env_lst;
 	while (temp)
@@ -35,4 +48,60 @@ char *get_env_value(t_env_list *env_lst, char *name)
 		temp = temp->next;
 	}
 	return (NULL);
+}
+
+void	change_env_value(t_env_list *env_lst, char *name, char *value)
+{
+	t_env_list	*temp;
+
+	temp = env_lst;
+	while (temp)
+	{
+		if (ft_strcmp(temp->name, name) == 0)
+		{
+			free(temp->value);
+			temp->value = ft_strdup(value);
+			return ;
+		}
+		temp = temp->next;
+	}
+}
+
+void	delete_env_value(t_env_list *env_lst, char *name)
+{
+	t_env_list	*temp;
+	t_env_list	*prev;
+
+	temp = env_lst;
+	prev = NULL;
+	while (temp)
+	{
+		if (ft_strcmp(temp->name, name) == 0)
+		{
+			if (prev)
+				prev->next = temp->next;
+			else
+				env_lst = temp->next;
+			free(temp->name);
+			free(temp->value);
+			free(temp);
+			return ;
+		}
+		prev = temp;
+		temp = temp->next;
+	}
+}
+
+void	free_env_lst(t_env_list *env_lst)
+{
+	t_env_list	*temp;
+
+	while (env_lst)
+	{
+		temp = env_lst;
+		env_lst = env_lst->next;
+		free(temp->name);
+		free(temp->value);
+		free(temp);
+	}
 }
