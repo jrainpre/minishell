ae62c686b421109dadf21486f0ac34f18c7da688
Marcel Koller <mkoller@c2r5p13.42vienna.com>
2023-01-30 16:13:56 +0100

    norm and function

--- fd_redirections/fd_redirect_in_helper2.c
+++ fd_redirections/fd_redirect_in_helper2.c
@@ -6,7 +6,7 @@
 /*   By: mkoller <mkoller@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 11:31:02 by jrainpre          #+#    #+#             */
-/*   Updated: 2023/01/30 15:39:54 by mkoller          ###   ########.fr       */
+/*   Updated: 2023/01/30 16:13:17 by mkoller          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	print_warning(char *limit)
 {
-	put_error("minishell: warning: here-document");
+	put_error("minishell: warning: here-document ");
 	put_error("delimited by end-of-file (wanted `");
 	put_error(limit);
 	put_error("')\n");
